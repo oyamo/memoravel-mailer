0e275cd7d1ee39361ae228c6229f2af2c09c6d57
oyamoh-brian <oyamobrian8@gmail.com>
2022-07-05 13:23:08 +0300

    e add cors

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+  res.header({"Access-Control-Allow-Origin": "*"});
+  next();
+})
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
