81120609058dc6e75aefad2904dc8ccf8d9f4039
oyamo <oyamo.xyz@gmail.com>
2023-01-01 21:36:18 +0300

    test-without-mailer

--- routes/contact.js
+++ routes/contact.js
@@ -8,21 +8,23 @@ router.post('/', async function (req, res) {
     const contact_details = req.body;
 
     if (contact_details.name && contact_details.email && contact_details.message) {
-        ejs.renderFile(path.join(__dirname, 'views/contact.ejs'), contact_details,  function (err, html) {
-            if (err) {
-                console.log(err);
-                res.status(500).send('Internal Server Error');
-            } else {
-                // await sendMail({
-                //     from: 'Memoravel Notifications',
-                //     to: 'oyamobrian8@gmail.com',
-                //     subject: 'New Message',
-                //     text: html
-                // })
+        // ejs.renderFile(path.join(__dirname, 'views/contact.ejs'), contact_details,  function (err, html) {
+        //     if (err) {
+        //         console.log(err);
+        //         res.status(500).send('Internal Server Error');
+        //     } else {
+        //         await sendMail({
+        //             from: 'Memoravel Notifications',
+        //             to: 'oyamobrian8@gmail.com',
+        //             subject: 'New Message',
+        //             text: html
+        //         })
 
-                res.status(200).send('Email sent successfully');
-            }
-        });
+        //         res.status(200).send('Email sent successfully');
+        //     }
+        // });
+        res.status(200).send( path.join(__dirname, 'views/contact.ejs'))
+       
     } else {
         res.status(400).json({
             message: 'Please provide all the required fields'
