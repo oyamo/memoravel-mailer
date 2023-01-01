dfbbcb9e4c85315ab0444d033758fbee885a87ae
oyamo <oyamo.xyz@gmail.com>
2023-01-01 20:50:36 +0300

    test-without-mailer

--- routes/contact.js
+++ routes/contact.js
@@ -4,26 +4,26 @@ const path = require('path');
 const sendMail = require('../src/mailer');
 var router = express.Router();
 
-router.post('/' ,function(req, res){
+router.post('/', async function (req, res) {
     const contact_details = req.body;
 
     if (contact_details.name && contact_details.email && contact_details.message) {
-       ejs.renderFile(path.join(__dirname, '../views/contact.ejs'), contact_details, async function (err, html) {
-           if (err) {
-               console.log(err);
-               res.status(500).send('Internal Server Error');
-           } else {
-               await sendMail({
-                   from: 'Memoravel Notifications',
-                   to: 'oyamobrian8@gmail.com',
-                   subject: 'New Message',
-                   text: html
-               })
+        ejs.renderFile(path.join(__dirname, '../views/contact.ejs'), contact_details, async function (err, html) {
+            if (err) {
+                console.log(err);
+                res.status(500).send('Internal Server Error');
+            } else {
+                // await sendMail({
+                //     from: 'Memoravel Notifications',
+                //     to: 'oyamobrian8@gmail.com',
+                //     subject: 'New Message',
+                //     text: html
+                // })
 
                 res.status(200).send('Email sent successfully');
-           }
-       })
-       } else {
+            }
+        });
+    } else {
         res.status(400).json({
             message: 'Please provide all the required fields'
         });
